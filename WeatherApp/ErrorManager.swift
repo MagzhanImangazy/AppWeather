f2f78d7c27f4e73c9bce2d534df21828859218c7
Magzhan Imangazin <magzhanimangazin@MacBook-Pro.local>
2019-06-12 18:35:42 +0600

    first commit

---
+++ WeatherApp/ErrorManager.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorManager.swift
+//  WeatherApp
+//
+//  Created by Imangazy Magzhan on 24/11/18.
+//  Copyright © 2018 Imangazy Magzhan. All rights reserved.
+//
+
+import Foundation
+
+public let SWINetworkingErrorDomain = "WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
+public let UnexpectedResponseError = 200
